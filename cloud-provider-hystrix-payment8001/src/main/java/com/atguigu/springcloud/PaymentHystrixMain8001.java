8f2de91985634a4d4782fc43cbcdcd884dcc3aa9
Joker <1104838667@qq.com>
2020-06-12 13:00:27 +0800

    fvvffv

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment.class,args);
+    }
+}
