7b092c0494a4cdc27e30b7d3358c49aa7975dc3c
Joker <1104838667@qq.com>
2020-06-12 23:26:41 +0800

    fvvffv

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +25,20 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "15000")
+    }
+    )
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
 
         String s = paymentHystrixService.paymentInfo_TimeOut(id);
         System.out.println(s);
         return s;
+    }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+
+        return "我是消费者80，对方支付系统繁忙，请10秒后重试，或者检查自己运行出错检查自己";
     }
 
 
